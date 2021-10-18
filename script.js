7a7e977c7c2673f4f79d429e6140271e9fa255c3
Chimwemwe-127001 <sinyinzachimwemwe@gmail.com>
2021-10-18 14:45:23 +0200

    Add item to local storage

--- script.js
+++ script.js
@@ -9,49 +9,56 @@ const book = function (title, author){
   this.author = author;
 }
 
-
 //books stored in array
 let booksListArr = [];
-
-
-
 
 function addBook(){
   bookList.innerHTML +=`
   <div class="book">
-      <h2>Book: ${title.value}</h2>
-      <h3>Author: ${author.value}</h3>
+      <h2 id ="titleVal">Book: ${title.value}</h2>
+      <h3 id ="authorVal">Author: ${author.value}</h3>
       <button type="button" class="remove">Remove</button>
       <hr>
     </div>  
   ` 
 }
 
+// document.addEventListener('DOMContentLoaded', () => {
+//   if(bookList !== null) {
+//     booksListArr = [JSON.parse(localStorage.getItem('bookList'))]
+//     booksListArr.forEach(e => {
+//       addBook(e)
+//     })
+//   }
+// })
+
 function buttonClick(){
   const newBook = new book(title.value, author.value);
   booksListArr.push(newBook);
   addBook(newBook);
+
+  localStorage.setItem('bookList', JSON.stringify(booksListArr))
 }
 
+document.addEventListener('DOMContentLoaded', () => {
+  if (bookList !== null) {
+    addBook()
+    let grabforminfo = localStorage.getItem('bookList');
+    grabforminfo = [...JSON.parse(grabforminfo)];
+    for(let i = 0; i < grabforminfo.length; i++) {
+      document.getElementById('titleVal').textContent = grabforminfo[i].title
+      document.getElementById('authorVal').textContent = grabforminfo[i].author;
+    }
+  }
+});
+
+
 addButton.addEventListener('click', buttonClick)
-
-
-
 
 bookList.addEventListener('click', (bk) => {
   if(bk.target.classList.contains('remove')){
     const bookD = bk.target.parentElement;
     bookList.removeChild(bookD);
     console.log("yay")
-    
-
-
   }
 })
-
-
-
-
-// removeBtn.forEach((bk) => {
-//   bk.addEventListener('click',removefunction)
-// })
