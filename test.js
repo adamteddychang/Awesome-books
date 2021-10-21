e2296fa6895a0bce51d324b50df1bb88929ee23a
adamteddychang <adamchang0725@gmail.com>
2021-10-21 11:36:50 +0200

    Fixed linter issues

--- test.js
+++ test.js
@@ -1,10 +1,3 @@
-var DateTime = luxon.DateTime; //--> Luxon's date object
-var localTime=DateTime.local(); // --> To get the current time
-
-const now = DateTime.now();
-
-document.getElementById("date-time").innerHTML = now
-
 const List = document.getElementById('List');
 const addNew = document.getElementById('Add-new');
 const Contact = document.getElementById('Contact');
@@ -12,23 +5,20 @@ const bookdisplay = document.getElementById('bookdisplay');
 const Addbook = document.getElementById('Addbook');
 const contactSec = document.getElementById('contact-sec');
 
-
-
 List.addEventListener('click', () => {
-    Addbook.classList.add('d-no');
-    bookdisplay.classList.remove('d-no');
-    contactSec.classList.add('d-no')
-})
+  Addbook.classList.add('d-no');
+  bookdisplay.classList.remove('d-no');
+  contactSec.classList.add('d-no');
+});
 
 addNew.addEventListener('click', () => {
-    Addbook.classList.remove('d-no');
-    bookdisplay.classList.add('d-no');
-    contactSec.classList.add('d-no')
-})
+  Addbook.classList.remove('d-no');
+  bookdisplay.classList.add('d-no');
+  contactSec.classList.add('d-no');
+});
 
 Contact.addEventListener('click', () => {
-    Addbook.classList.add('d-no');
-    bookdisplay.classList.add('d-no');
-    contactSec.classList.remove('d-no')
-})
-
+  Addbook.classList.add('d-no');
+  bookdisplay.classList.add('d-no');
+  contactSec.classList.remove('d-no');
+});
